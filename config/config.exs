0acff0bd86492670d6b1ea3e22fff68ee6c479a3
Rahul <rahul@dbadbadba.com>
2022-04-04 15:12:08 +0530

    moved constants to their files

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,7 @@
 use Mix.Config
 config :tesla, adapter: Tesla.Adapter.Hackney
+
+config :zerodha,
   base_url: "https://zerodha-sandbox.herokuapp.com"
+
+# Move this url as ENV variable
--- lib/zerodha/constants.ex
+++ lib/zerodha/constants.ex
@@ -1,71 +1,70 @@
 defmodule Zerodha.Constants do
-
   @default_root_uri "https://api_kite_trade"
   @default_login_uri "https://kite_trade/connect/login"
   @default_timeout 7000
 
-  # Constants
-  # products
-  @product_MIS "MIS"
-  @product_CNC "CNC"
-  @product_NRML "NRML"
-  @product_CO "CO"
-  @product_BO "BO"
+  # # Constants
+  # # products
+  # @product_MIS "MIS"
+  # @product_CNC "CNC"
+  # @product_NRML "NRML"
+  # @product_CO "CO"
+  # @product_BO "BO"
 
-  # order types
-  @order_type_market "MARKET"
-  @order_type_limit "LIMIT"
-  @order_type_slm "SL-M"
-  @order_type_sl "SL"
+  # # order types
+  # @order_type_market "MARKET"
+  # @order_type_limit "LIMIT"
+  # @order_type_slm "SL-M"
+  # @order_type_sl "SL"
 
-  # Varities
-  @variety_REGULAR "regular"
-  @variety_BO "bo"
-  @variety_CO "co"
-  @variety_AMO "amo"
+  # # Varities
+  # @variety_REGULAR "regular"
+  # @variety_BO "bo"
+  # @variety_CO "co"
+  # @variety_AMO "amo"
 
-  # transaction type
-  @transaction_type_buy "BUY"
-  @transaction_type_sell "SELL"
+  # # transaction type
+  # @transaction_type_buy "BUY"
+  # @transaction_type_sell "SELL"
 
-  # validity
-  @validity_day "DAY"
-  @validity_IOC "IOC"
+  # # validity
+  # @validity_day "DAY"
+  # @validity_IOC "IOC"
 
-  # position type
-  @position_type_day "day"
-  @position_type_overnight "overnight"
+  # # position type
+  # @position_type_day "day"
+  # @position_type_overnight "overnight"
 
-  # exchanges
-  @exchange_NSE "NSE"
-  @exchange_BSE "BSE"
-  @exchange_NFO "NFO"
-  @exchange_CDS "CDS"
-  @exchange_BFO "BFO"
-  @exchange_MCX "MCX"
-  @exchange_BCD "BCD"
+  # # exchanges
+  # @exchange_NSE "NSE"
+  # @exchange_BSE "BSE"
+  # @exchange_NFO "NFO"
+  # @exchange_CDS "CDS"
+  # @exchange_BFO "BFO"
+  # @exchange_MCX "MCX"
+  # @exchange_BCD "BCD"
 
-  # Margins segments
-  @margin_equity "equity"
-  @margin_commodity "commodity"
+  # # Margins segments
+  # @margin_equity "equity"
+  # @margin_commodity "commodity"
 
-  # status constants
-  @status_complete "COMPLETE"
-  @status_rejected "REJECTED"
-  @status_cancelled "CANCELLED"
+  # # status constants
+  # @status_complete "COMPLETE"
+  # @status_rejected "REJECTED"
+  # @status_cancelled "CANCELLED"
 
-  # gtt order type
-  @gtt_type_OCO "two-leg"
-  @gtt_type_SINGLE "single"
+  # # gtt order type
+  # @gtt_type_OCO "two-leg"
+  # @gtt_type_SINGLE "single"
 
-  # gtt order status
-  @gtt_status_active "active"
-  @gtt_status_triggered "triggered"
-  @gtt_status_disabled "disabled"
-  @gtt_status_expired "expired"
-  @gtt_status_cancelled "cancelled"
-  @gtt_status_rejected "rejected"
-  @gtt_status_deleted "deleted"
+  # # gtt order status
+  # @gtt_status_active "active"
+  # @gtt_status_triggered "triggered"
+  # @gtt_status_disabled "disabled"
+  # @gtt_status_expired "expired"
+  # @gtt_status_cancelled "cancelled"
+  # @gtt_status_rejected "rejected"
+  # @gtt_status_deleted "deleted"
 
   @routes %{
     api_token: "/session/token",
@@ -119,4 +118,69 @@ defmodule Zerodha.Constants do
   }
 
   def routes, do: @routes
+
+  def default_root_url, do: @default_root_uri
+  def default_login_uri, do: @default_login_uri
+  def default_timeout, do: @default_timeout
+  def product_MIS, do: "MIS"
+  def product_CNC, do: "CNC"
+
+  def product_NRML, do: "NRML"
+  def product_CO, do: "CO"
+  def product_BO, do: "BO"
+
+  # order types
+  def order_type_market, do: "MARKET"
+  def order_type_limit, do: "LIMIT"
+  def order_type_slm, do: "SL-M"
+  def order_type_sl, do: "SL"
+
+  # Varities
+  def variety_REGULAR, do: "regular"
+  def variety_BO, do: "bo"
+  def variety_CO, do: "co"
+  def variety_AMO, do: "amo"
+
+  # transaction type
+  def transaction_type_buy, do: "BUY"
+  def transaction_type_sell, do: "SELL"
+
+  # validity
+  def validity_day, do: "DAY"
+  def validity_IOC, do: "IOC"
+
+  # position type
+  def position_type_day, do: "day"
+  def position_type_overnight, do: "overnight"
+
+  # exchanges
+  def exchange_NSE, do: "NSE"
+  def exchange_BSE, do: "BSE"
+  def exchange_NFO, do: "NFO"
+  def exchange_CDS, do: "CDS"
+  def exchange_BFO, do: "BFO"
+  def exchange_MCX, do: "MCX"
+  def exchange_BCD, do: "BCD"
+
+  # Margins segments
+  def margin_equity, do: "equity"
+  def margin_commodity, do: "commodity"
+
+  # status constants
+  def status_complete, do: "COMPLETE"
+  def status_rejected, do: "REJECTED"
+  def status_cancelled, do: "CANCELLED"
+
+  # gtt order type
+  def gtt_type_OCO, do: "two-leg"
+  def gtt_type_SINGLE, do: "single"
+
+  # gtt order status
+  def gtt_status_active, do: "active"
+  def gtt_status_triggered, do: "triggered"
+  def gtt_status_disabled, do: "disabled"
+  def gtt_status_expired, do: "expired"
+  def gtt_status_cancelled, do: "cancelled"
+  def gtt_status_rejected, do: "rejected"
+  def gtt_status_deleted, do: "deleted"
 end
