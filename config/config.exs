9c545fca54007cacdd9aa2ade59a1748ecd12ed0
Rahul <rahul@dbadbadba.com>
2022-04-10 16:43:34 +0530

    api keys in config

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,10 @@ use Mix.Config
 config :tesla, adapter: Tesla.Adapter.Hackney
 
 config :zerodha,
-  api_key: "testst",
-  base_url: "https://zerodha-sandbox.herokuapp.com"
+  api_key: System.get_env("API_KEY"),
+  api_secret: System.get_env("API_SECRET"),
+  base_url: "https://api.kite.trade"
+
+# config :trading,
 
 # Move this url as ENV variable
